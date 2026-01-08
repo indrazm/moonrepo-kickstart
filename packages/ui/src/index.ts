cf2ff783354298ac75edf33175b155ae4aa96f18
Indra Zulfi Mushoddaq <me@indrazm.com>
2026-01-09 01:05:19 +0700

    adding shadcn ui

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,20 @@
+export { Button, buttonVariants } from "./components/ui/button";
+export { Input } from "./components/ui/input";
+export { Textarea } from "./components/ui/textarea";
+export { Label } from "./components/ui/label";
+export { Checkbox } from "./components/ui/checkbox";
+export { RadioGroup, RadioGroupItem } from "./components/ui/radio-group";
+export { Switch } from "./components/ui/switch";
+export {
+	Select,
+	SelectGroup,
+	SelectValue,
+	SelectTrigger,
+	SelectContent,
+	SelectLabel,
+	SelectItem,
+	SelectSeparator,
+	SelectScrollUpButton,
+	SelectScrollDownButton,
+} from "./components/ui/select";
+export { cn } from "./lib/utils";
